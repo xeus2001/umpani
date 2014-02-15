e018a51ac7b05bb83171641c45d38faa20ca7631
Alexander Weber <xeus2001@gmail.com>
2014-02-15 03:09:45 +0100

    Modified the code so that tries to collect a ceratin amount of prime numbers, but aborts after a maximal time given.

--- util/src/main/java/com/umpani/util/playground/PrimeGen.java
+++ util/src/main/java/com/umpani/util/playground/PrimeGen.java
@@ -22,115 +22,136 @@ public class PrimeGen {
 	}
 	public static final long LONG_ARRAY_OFFSET = unsafe.arrayBaseOffset(long[].class);
 	public static final long LONG_ARRAY_SCALE = unsafe.arrayIndexScale(long[].class);
-	
-	public static final int BLOCK_SHIFT = 16;
+	public static final int BLOCK_SHIFT = 12;
 	public static final long BLOCK_SIZE = 1 << BLOCK_SHIFT;
 
 	/**
 	 * Returns the first primes unordered.
 	 * @param primes
 	 * the array to fill with primes, starting with 1. The array must be filled with 0.
+	 * @param maxTime
+	 * the maximal time to be consumed.
+	 * @param maxTimeUnit
+	 * the time unit in which the maxTime was provided.
+	 * @return
+	 * the found primes.
 	 */
-	public static final void getPrimes( final long[] thePrimes ) {
+	public static final long[] getPrimes( final long[] thePrimes, final long maxTime, final TimeUnit maxTimeUnit ) {
 		// we know the first three prime numbers
 		thePrimes[0] = 2L; // we fix that later
 		thePrimes[1] = 2L;
 		thePrimes[2] = 3L;
 
 		// search for primes using multiple threads
+		final long STOP_TIME = System.currentTimeMillis() + maxTimeUnit.toMillis(maxTime);
 		final int threadCount = Runtime.getRuntime().availableProcessors();
 		final Thread[] threads = new Thread[threadCount];
 		final AtomicLong nextBlock = new AtomicLong(0);
 		final boolean[] processed = new boolean[1024*1024];
-		final Object doneMutex = new Object();
-		synchronized (doneMutex) {
-			for (int i=0; i < threads.length; i++) {
-				threads[i] = new Thread() {
-					@Override
-					public void run() {
-						long lastKnownValidatedPrime = 3;
-						int lastKnownValidatedPrimeIndex = 2;
-						primefind: while (true) {
-							final long start = nextBlock.getAndAdd(BLOCK_SIZE);
-							final long end = start + BLOCK_SIZE;
-							long prime = start < 5 ? 5 : start+1; // start with odd number!
-							search: while (prime < end) {
-								// maximal and minimal divisor to test for this prime
-								final long max = (long)Math.ceil(Math.sqrt(prime));
+		for (int i=0; i < threads.length; i++) {
+			threads[i] = new Thread() {
+				@Override
+				public void run() {
+					long lastKnownValidatedPrime = 3;
+					int lastKnownValidatedPrimeIndex = 2;
+					primefind: while (true) {
+						if (System.currentTimeMillis() >= STOP_TIME) break;
+						final long start = nextBlock.getAndAdd(BLOCK_SIZE);
+						final long end = start + BLOCK_SIZE;
+						long prime = start < 5 ? 5 : start+1; // start with odd number!
+						search: while (prime < end) {
+							// maximal and minimal divisor to test for this prime
+							final long max = (long)Math.ceil(Math.sqrt(prime));
 
-								// try to divide the number by all already found primes (quick exclusion)
-								int thePrimesIndex = 0;
-								for (; thePrimesIndex < thePrimes.length; thePrimesIndex++) {
-									long nextFoundPrime = thePrimes[thePrimesIndex];
-									if (nextFoundPrime==0) break;
+							// try to divide the number by all already found primes (quick exclusion)
+							int thePrimesIndex = 0;
+							for (; thePrimesIndex < thePrimes.length; thePrimesIndex++) {
+								long nextFoundPrime = thePrimes[thePrimesIndex];
+								if (nextFoundPrime==0) break;
 
-									// if it is dividable, it is not prime
-									if ((prime % nextFoundPrime)==0) {
-										prime+=2; // we don't test even numbers
-										continue search;
-									}
+								// if it is dividable, it is not prime
+								if ((prime % nextFoundPrime)==0) {
+									prime+=2; // we don't test even numbers
+									continue search;
 								}
-
-								// if the number is not dividable by any previously found prime it might be prime
-								
-								// test all unvalidated primes
-								for (long i=lastKnownValidatedPrime; i <= max; i+=2) { // we can skip even numbers
-									// if it is dividable, it is not prime
-									if ((prime % i)==0) {
-										prime+=2; // we don't test even numbers
-										continue search;
-									}
-								}
-
-								// we found a new prime, write it back into the list of found primes
-								for (int i=thePrimesIndex; i < thePrimes.length; i++) {
-									long nextFoundPrime = thePrimes[i];
-									if (nextFoundPrime==0 &&
-										unsafe.compareAndSwapLong(thePrimes,LONG_ARRAY_OFFSET+i*LONG_ARRAY_SCALE, 0, prime)
-									) {
-										// done
-										prime+=2; // we don't test even numbers
-										continue search;
-									}
-								}
-								// if we reached this point the primes array is full, so we're done with the search
-								break primefind;
 							}
 
-							// tell other threads that the primes of this thread are validated
-							processed[(int)(start>>>BLOCK_SHIFT)] = true;
+							// if the number is not dividable by any previously found prime it might be prime
 							
-							// find the value of the biggest validated number
-							long biggestValidatedNumber = lastKnownValidatedPrime >>> BLOCK_SHIFT;
-							for (int i=(int)biggestValidatedNumber; i < processed.length; i++) {
-								if (!processed[i]) break;
-								biggestValidatedNumber += BLOCK_SIZE;
-							}
-
-							// find the biggest validated prime number below the biggest validates number
-							for (int i=lastKnownValidatedPrimeIndex; i < thePrimes.length; i++) {
-								final long validatedPrim = thePrimes[i];
-								if (validatedPrim==0 || validatedPrim > biggestValidatedNumber) break;
-								if (validatedPrim > lastKnownValidatedPrime) {
-									lastKnownValidatedPrime = validatedPrim;
-									lastKnownValidatedPrimeIndex = i;
+							// test all unvalidated primes
+							for (long i=lastKnownValidatedPrime; i <= max; i+=2) { // we can skip even numbers
+								// if it is dividable, it is not prime
+								if ((prime % i)==0) {
+									prime+=2; // we don't test even numbers
+									continue search;
 								}
 							}
+
+							// we found a new prime, write it back into the list of found primes
+							for (int i=thePrimesIndex; i < thePrimes.length; i++) {
+								long nextFoundPrime = thePrimes[i];
+								if (nextFoundPrime==0 &&
+									unsafe.compareAndSwapLong(thePrimes,LONG_ARRAY_OFFSET+i*LONG_ARRAY_SCALE, 0, prime)
+								) {
+									// done
+									prime+=2; // we don't test even numbers
+									continue search;
+								}
+							}
+							// if we reached this point the primes array is full, so we're done with the search
+							break primefind;
 						}
-						synchronized (doneMutex) {
-							doneMutex.notifyAll();
+
+						// tell other threads that the primes of this thread are validated
+						processed[(int)(start>>>BLOCK_SHIFT)] = true;
+						
+						// find the value of the biggest validated number
+						long biggestValidatedNumber = lastKnownValidatedPrime >>> BLOCK_SHIFT;
+						for (int i=(int)biggestValidatedNumber; i < processed.length; i++) {
+							if (!processed[i]) break;
+							biggestValidatedNumber += BLOCK_SIZE;
+						}
+
+						// find the biggest validated prime number below the biggest validates number
+						for (int i=lastKnownValidatedPrimeIndex; i < thePrimes.length; i++) {
+							final long validatedPrim = thePrimes[i];
+							if (validatedPrim==0 || validatedPrim > biggestValidatedNumber) break;
+							if (validatedPrim > lastKnownValidatedPrime) {
+								lastKnownValidatedPrime = validatedPrim;
+								lastKnownValidatedPrimeIndex = i;
+							}
 						}
 					}
-				};
-				threads[i].start();
-			}
+				}
+			};
+			threads[i].start();
+		}
+		for (int i=0; i < threads.length; i++) {
 			try {
-				doneMutex.wait();
+				Thread.interrupted();
+				threads[i].join();
 			} catch (InterruptedException e) {}
 		}
 		thePrimes[0]=1L;
+		
+		// if the array is full, return it
+		int i=thePrimes.length-1;
+		if (thePrimes[i]!=0) {
+			Arrays.sort(thePrimes);
+			return thePrimes;
+		}
+		
+		// otherwise truncate it
+		for (;i >= 0; i--) {
+			if (thePrimes[i]!=0) break;
+		}
+		final long[] theFinalPrimes = Arrays.copyOf(thePrimes,i);
+		Arrays.sort(theFinalPrimes);
+		return theFinalPrimes;
 	}
-	public static void findPrimes( final int amount ) {
+	public static void findPrimes(
+		final int amount, final long maxTime, final TimeUnit maxTimeUnit, final boolean showResult
+	) {
 		final WeakReference<Object> gcMe = new WeakReference<Object>( new Object() );
 		while (gcMe.get()!=null) {
 			System.gc();
@@ -140,21 +161,24 @@ public class PrimeGen {
 				Thread.interrupted();
 			}
 		}
-		final long[] array = new long[amount];
+		long[] array = new long[amount];
 		final long start = System.nanoTime();
-			getPrimes(array);
+			array = getPrimes(array,maxTime,maxTimeUnit);
 		final long end = System.nanoTime();
-		Arrays.sort(array);
-		for (int i=0; i < array.length; i++) {
-			System.out.println("prime #"+i+" = "+array[i]);
+		if (showResult) {
+			for (int i=0; i < array.length; i++) {
+				System.out.println("prime #"+i+" = "+array[i]);
+			}
+			System.out.println("time consumed: "+TimeUnit.NANOSECONDS.toMillis(end-start)+"ms");
 		}
-		System.out.println("time consumed: "+TimeUnit.NANOSECONDS.toMillis(end-start)+"ms");
 	}
 
 	public static void main(String... args) {
 		// warmup
-		findPrimes(20_000);
-		// performance test, search for the first 50,000 prime numbers
-		findPrimes(200_000);
+		findPrimes(10_000,10,TimeUnit.SECONDS,false);
+		findPrimes(100_000,1,TimeUnit.SECONDS,false);
+
+		// performance test
+		findPrimes(100_000,1,TimeUnit.SECONDS,true);
 	}
 }
